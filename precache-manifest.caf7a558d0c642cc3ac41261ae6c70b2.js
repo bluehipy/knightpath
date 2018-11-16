7f7a81ea698448b21037c43d6934b4b73963e098
bluehipy <bluehipy@yahoo.com>
2018-11-16 10:33:13 +0100

    paths

---
+++ precache-manifest.caf7a558d0c642cc3ac41261ae6c70b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "270bde308b3680d16aff",
+    "url": "/knightpath/static/js/runtime~main.270bde30.js"
+  },
+  {
+    "revision": "97e9e6a5b5a6f2bcd00e",
+    "url": "/knightpath/static/js/main.97e9e6a5.chunk.js"
+  },
+  {
+    "revision": "31263b632d0e0743f138",
+    "url": "/knightpath/static/js/1.31263b63.chunk.js"
+  },
+  {
+    "revision": "97e9e6a5b5a6f2bcd00e",
+    "url": "/knightpath/static/css/main.a19ba449.chunk.css"
+  },
+  {
+    "revision": "6f9ace5b02e4783801af4b835aa68c86",
+    "url": "/knightpath/index.html"
+  }
+];
