7825b185094eb8176ca16191d593e1f2589114fc
bluehipy <bluehipy@yahoo.com>
2018-11-16 06:13:54 +0100

    init page

--- src/components/Utils.js
+++
@@ -1,9 +0,0 @@
-export const toCartezian = (s) => {
-  const pos = s.toLowerCase().split(""),
-    alpha = "abcdefgh";
-  return [9 - parseInt(pos[1], 10), 1 + alpha.indexOf(pos[0])];
-}
-export const toChess = (x, y) => {
-  const alpha = "abcdefgh".split("");
-  return alpha[y - 1] + (9 - x);
-}
